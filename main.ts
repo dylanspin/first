de93c4d130c825993c701ccdee624cb357a26736
dylanspin <47520114+dylanspin@users.noreply.github.com>
2021-09-13 10:54:50 +0200

    should work?

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (true) {
-    	
+radio.onReceivedString(function (receivedString) {
+    singaal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    if (singaal >= minimalDistance) {
+        basic.showString("good enough")
     } else {
-    	
+        basic.showLeds(`
+            . . # # .
+            . # . . .
+            . . # # .
+            . . . . #
+            . . # # .
+            `)
     }
 })
+let singaal = 0
+let minimalDistance = 0
 radio.sendNumber(1)
 radio.setTransmitPower(1)
+minimalDistance = 75
 basic.forever(function () {
-    radio.sendString("1")
+    radio.sendString("test")
     basic.pause(200)
 })
